3039d2c8892e5f049ae9e92f6b5df4d700cd6f05
fenrir <fenrir.naru@gmail.com>
2018-08-15 11:22:25 +0900

    Update and refactoring; RSpec recommends expect() instead of should()

--- spec/rinruby_spec.rb
+++ spec/rinruby_spec.rb
@@ -4,46 +4,40 @@ puts "RinRuby #{RinRuby::VERSION} specification"
 
 describe RinRuby do
   describe "on init" do
+    let(:params){
+      {
+        :echo_enabled => false, 
+        :interactive => false, 
+        :executable => nil, 
+        :port_number => 38500, 
+        :port_width => 1,
+      }
+    }
+    let(:r){
+      RinRuby.new(*([:echo_enabled, :interactive, :executable, :port_number, :port_width].collect{|k| params[k]}))
+    }
     it "should accept parameters as specified on Dahl & Crawford(2009)" do
-      
-      platform = case RUBY_PLATFORM
-      when /mswin/ then 'windows'
-      when /mingw/ then 'windows'
-      when /bccwin/ then 'windows'
-      else 
-        "other"
-      end
-      if platform=='windows'
-        skip("Difficult to test without specific location of R executable on Windows")
-      else      
-      
-      r=RinRuby.new(false, false, "R", 38500, 1)
-      
-      expect(r.echo_enabled).to be false
-      r.interactive.should be false
-      r.executable.should=="R"
-      r.port_number.should==38500
-      r.port_width.should==1      
-      end
+      expect(r.echo_enabled).to be_falsy
+      expect(r.interactive).to be_falsy
+      expect(r.port_number).to eq(params[:port_number])
+      expect(r.port_width).to eq(params[:port_width])
+      case r.instance_variable_get(:@platform)
+      when /^windows/ then
+        expect(r.executable).to match(/Rterm\.exe["']?$/)
+      else
+        expect(r.executable).to eq("R")
+      end      
     end
-    it "should accept :echo and :interactive parameters" do
-      r=RinRuby.new(:echo=>false, :interactive=>false)
-      r.echo_enabled.should be false
-      r.interactive.should be false
-      
+    it "should accept custom :port_number" do
+      params.merge!(:port_number => 38442+rand(3))
+      expect(r.port_number).to eq(params[:port_number])
     end
-    it "should accept :port_number" do
-      port=38442+rand(3)
-      r=RinRuby.new(:port_number=>port,:port_width=>1)
-      r.port_number.should==port
-      r.quit
-    end
-    it "should accept :port_width" do
-      port=38442
-      port_width=rand(10)+1
-      r=RinRuby.new(:port=>port, :port_width=>port_width)
-      expect(r.port_width).to be == port_width
-      r.port_number.should satisfy {|v| v>=port and v < port+port_width}
+    it "should accept custom :port_width" do
+      params.merge!(:port_number => 38442, :port_width => rand(10)+1)
+      expect(r.port_width).to eq(params[:port_width])
+      expect(r.port_number).to satisfy {|v| 
+        ((params[:port_number])...(params[:port_number] + params[:port_width])).include?(v)
+      }
     end
   end
   before do
@@ -51,82 +45,80 @@ describe RinRuby do
   end
   subject {R}
   context "basic methods" do 
-    it {should respond_to :eval}
-    it {should respond_to :quit}
-    it {should respond_to :assign}
-    it {should respond_to :pull}
-    it {should respond_to :quit}
-    it {should respond_to :echo}
+    it {is_expected.to respond_to(:eval)}
+    it {is_expected.to respond_to(:quit)}
+    it {is_expected.to respond_to(:assign)}
+    it {is_expected.to respond_to(:pull)}
+    it {is_expected.to respond_to(:quit)}
+    it {is_expected.to respond_to(:echo)}
     it "return correct values for complete?" do
-      R.eval("x<-1").should be true
+      expect(R.eval("x<-1")).to be_truthy
     end
     it "return false for complete? for incorrect expressions" do
-      R.complete?("x<-").should be false
+      expect(R.complete?("x<-")).to be_falsy
     end
     it "correct eval should return true" do 
-      R.complete?("x<-1").should be true
+      expect(R.complete?("x<-1")).to be_truthy
     end
     it "incorrect eval should raise an ParseError" do
-      lambda {R.eval("x<-")}.should raise_error(RinRuby::ParseError)
+      expect{R.eval("x<-")}.to raise_error(RinRuby::ParseError)
     end
   end
-  context "on assing" do 
+  context "on assing" do
+    let(:x){rand} 
     it "should assign correctly" do
-      x=rand
       R.assign("x",x)
-      R.pull("x").should==x
+      expect(R.pull("x")).to eq(x)
     end
     it "should be the same using assign than R#= methods" do
-      x=rand
       R.assign("x1",x)
       R.x2=x
-      R.pull("x1").should==x
-      R.pull("x2").should==x
+      expect(R.pull("x1")).to eq(x)
+      expect(R.pull("x2")).to eq(x)
     end
     it "should raise an ArgumentError error on setter with 0 parameters" do
-      lambda {R.unknown_method=() }.should raise_error(ArgumentError)
+      expect{R.unknown_method=() }.to raise_error(ArgumentError)
     end
-    
   end
   context "on pull" do
     it "should be the same using pull than R# methods" do
       x=rand
       R.x=x
-      R.pull("x").should==x
-      R.x.should==x
+      expect(R.pull("x")).to eq(x)
+      expect(R.x).to eq(x)
     end
     it "should raise an NoMethod error on getter with 1 or more parameters" do
-      lambda {R.unknown_method(1) }.should raise_error(NoMethodError)
+      expect{R.unknown_method(1) }.to raise_error(NoMethodError)
     end
     
     it "should pull a String" do
       R.eval("x<-'Value'")
-      R.pull('x').should=='Value'
+      expect(R.pull('x')).to eq('Value')
     end
     it "should pull an Integer" do
       R.eval("x<-1")
-      R.pull('x').should==1
+      expect(R.pull('x')).to eq(1)
     end
     it "should pull a Float" do
       R.eval("x<-1.5")
-      R.pull('x').should==1.5
+      expect(R.pull('x')).to eq(1.5)
     end
     it "should pull an Array of Numeric" do
       R.eval("x<-c(1,2.5,3)")
-      R.pull('x').should==[1,2.5,3]
+      expect(R.pull('x')).to eq([1,2.5,3])
     end
     it "should pull an Array of strings" do
       R.eval("x<-c('a','b')")
-      R.pull('x').should==['a','b']
+      expect(R.pull('x')).to eq(['a','b'])
     end
 
     it "should push a Matrix" do
       matrix=Matrix::build(100, 200){|i, j| rand} # 100 x 200 matrix
-      lambda {R.assign('x',matrix)}.should_not raise_error
+      expect{R.assign('x',matrix)}.not_to raise_error
       rx=R.x
       matrix.row_size.times {|i|
         matrix.column_size.times {|j|
-          matrix[i,j].should be_within(1e-10).of(rx[i,j])
+          expect(matrix[i,j]).to be_within(1e-10).of(rx[i,j])
         }
       }
     end
@@ -134,17 +126,13 @@ describe RinRuby do
   end
 
   context "on quit" do
-    before(:each) do
-      @r=RinRuby.new(:echo=>false)
-    end
+    let(:r){RinRuby.new(:echo=>false)}
     it "return true" do
-      @r.quit.should be true
+      expect(r.quit).to be_truthy
     end
     it "returns an error if used again" do
-      @r.quit
-      lambda {@r.eval("x=1")}.should raise_error(RinRuby::EngineClosed)
+      r.quit
+      expect{r.eval("x=1")}.to raise_error(RinRuby::EngineClosed)
     end
   end
-  
-
 end
